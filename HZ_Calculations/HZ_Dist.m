cc54cb93195ba25f612862c53de73f2b9ba89c34
Blake Wittmayer <89792296+BlakeWittmayer@users.noreply.github.com>
2022-04-18 11:52:24 -0500

    Create HZ_Dist.m

---
+++ HZ_Calculations/HZ_Dist.m
@@ -0,0 +1,21 @@
+% Import table of coefficients 
+coeff = readtable("Coeff.csv"); % table of coefficients
+coeff.Properties.VariableNames = {'Recent Venus' 'Runaway Greenhouse' 
+    'Maximum Greenhouse' 'Early Mars'};
+coeff.Properties.RowNames = {'S_effSun' 'a' 'b' 'c' 'd'};
+
+Tstar = T_eff - 5780; 
+S_eff = zeros(1,4); % Effective solar flux array [W m^-2]
+dist = zeros(1,4); % Habitable zone distance array [AU]
+
+for i = 1:1:4
+    S_effSun = coeff{1,i};
+    a = coeff{2,i};
+    b = coeff{3,i};
+    c = coeff{4,i};
+    d = coeff{5,i};
+
+    S_eff(1,i) = S_effSun+a*Tstar+b*Tstar^2+c*Tstar^3+d*Tstar^4; 
+    dist(1,i) =  (L/S_eff(1,i))^.5;
+end
+end
