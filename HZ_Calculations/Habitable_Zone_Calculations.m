41d5894f6da8f3596af2080ccf21669a1466b3dd
Blake Wittmayer <89792296+BlakeWittmayer@users.noreply.github.com>
2022-09-16 10:54:19 -0500

    Update Habitable_Zone_Calculations.m
    Moved functions to function folder

--- HZ_Calculations/Habitable_Zone_Calculations.m
+++ HZ_Calculations/Habitable_Zone_Calculations.m
@@ -26,31 +26,3 @@ plot(0,0,'.','Color','#EDB120','MarkerSize',30); % Star at origin
 hold on
 formatPlot(Star.B);
 hold off
-
-function circle(x,y,r,color)
-ang=0:0.01:2*pi; 
-xp=r*cos(ang);
-yp=r*sin(ang);
-plot(x+xp,y+yp,LineWidth=1,Color=color);
-end
-
-function formatPlot(s)
-title(append(s.Name,' Habitable Zone'));
-circle(0,0,s.HZ(1),'#FF0000');
-circle(0,0,s.HZ(2),'#EDB120');
-circle(0,0,s.HZ(3),'#77AC30');
-circle(0,0,s.HZ(4),'#0072BD');
-text(s.HZ(1)-0.025,0.15,'Recent Venus','Color','#FF0000','FontSize',15,'HorizontalAlignment','right');
-text(s.HZ(2),0.25,'Runaway Greenhouse','Color','#EDB120','FontSize',15);
-text(s.HZ(3)-0.025,0.15,'Maximum Greenhouse','Color','#77AC30','FontSize',15,'HorizontalAlignment','right');
-text(s.HZ(4),0.25,'Early Mars','Color','#0072BD','FontSize',15);
-text(0,0.1,s.Name,'FontSize',15);
-ax = gca; % Get handle to current axes.
-ax.XAxisLocation = 'origin';
-ax.TitleFontSizeMultiplier = 2.25;
-ax.YLim = [-0.5 0.5];
-ax.XLim = [-0.5 5];
-ax.YAxis.Visible = 'off';
-ax.FontSize = 15;
-xlabel('Astronomical Units [au]','FontSize',15);
-end
