403fb17992e2a5260343a3172ebe3c22b32e8ad4
Blake Wittmayer <blake.wittmayer@mavs.uta.edu>
2022-10-10 14:43:29 -0500

    Revert "Delete BinHab/Functions directory"
    This reverts commit 83372331527fc7f165bd071093514a7dfcbd4261.

---
+++ BinHab/Functions/Lprime_il.m
@@ -0,0 +1,3 @@
+function Lp_il = Lprime_il(L,s_l,T_eff)
+    Lp_il = L/S_rel(s_l,T_eff);
+end
---
+++ BinHab/Functions/inputHZB.m
@@ -0,0 +1,27 @@
+function [l_in,l_out] = inputHZB(in,out)
+T = readtable('Table 2.csv'); % Habitability Limits for the Sun
+clc;
+disp(T);
+while true
+    % in = input("Choose an inner boundary:\n");
+    if in == 4
+        fprintf("Selected choice is not a boundary\n");
+    elseif (in > 4 || in < 1)
+        fprintf("Select a boundary within the range of 1-3\n");
+    else
+        l_in = table2array(T(in,2));
+        break
+    end
+end
+while true
+    % out = input("Choose an outer boundary:\n");
+    if out == 4
+        fprintf("Selected choice is not a boundary\n");
+    elseif (out > 7 || out < 4)
+        fprintf("Select a boundary within the range of 5-7\n");
+    else
+        l_out = table2array(T(out,2));
+        break
+    end
+end
+end
