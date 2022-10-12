7b6f0b4ee047c2c559e242cbbe03ff7591b7315c
Blake Wittmayer <blake.wittmayer@mavs.uta.edu>
2022-10-12 15:08:03 -0500

    Update Orbital_Stability.py

--- Hill_Stability/Orbital_Stability.py
+++ Hill_Stability/Orbital_Stability.py
@@ -10,13 +10,19 @@ University of Texas at Arlington,
 Arlington, TX 76019, USA
 """
 
-###############################################
-# Functions
-###############################################
+# This function inputs the mass of the parent body and child body, M and m, 
+# respectively, as well as the semi-major axis, a. Units are in solar mass
+# and astronomical units (au). The returned value is the Hill radius in units
+# of au as defined by Hamiltion & Burns 1992.
 def hillRadius(a,m,M):
     rH = a*(m/(3*M))**(1/3)
     return rH
 
+# This function returns the Hill stability criterion as defined Boyle & Cuntz
+# 2021. The input values are the masses of the binary parent body and child 
+# body, m1 and m2, respectively, with m3 being equal to the third component of
+# the system, m3. Input values are in units of solar mass while the return 
+# values are in units of au.
 def hill3Body(m1,m2,m3):
     M2 = m1 + m2
     B1 = 1.48035831
@@ -28,13 +34,15 @@ def hill3Body(m1,m2,m3):
     crr = B3*y*(1-B4*y)
     return crp,crr;
 
+# This function displays the boolean statement if the system is Hill stable
+# by comparing a to crr. The comparison is printed in the console.
 def hillStable(a,crr):
     if a <= crr:
         print("\u03B1 <= \u03B1_crr\nSystem is stable")
     elif a > crr:
         print("\u03B1 > \u03B1_crr\nSystem is not stable")
 
-###############################################
+
 # Input Data
 ###############################################
 # Stellar masses of components [Solar Mass, M*]
@@ -45,7 +53,6 @@ m3 = 0.50       # 30 Ari C
 a1 = 0.967      # Primary Ba & companion Bb
 a2 = 21.9       # Primary B & companion C
 
-###############################################
 # Calculations
 ###############################################
 M = m1 + m2
@@ -54,10 +61,8 @@ a = a1/a2
 rH1 = hillRadius(a1,m2,m1)
 rH2 = hillRadius(a2,m3,M)
 
-###############################################
 # Table
 ###############################################
-# Build table
 from tabulate import tabulate as tabl
 SystemInfo = [["m1 (M*)",m1],["m2 (M*)",m2],["m3 (M*)",m3],["M  (M*)",M],
               ["a1 (au)",a1],["a2 (au)",a2]]
@@ -65,7 +70,6 @@ StelComp = [["\u03B1",a],["\u03B1_crp",crp],["\u03B1_crr",crr],
             ["Bb rH (au)",rH1],["C rH (au)",rH2]]
 headers = ["Parameter","Data"]
 
-###############################################
 # Output Data
 ###############################################
 print(" System Information")
